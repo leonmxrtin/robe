c508fbfeb7b58a03c144aa9d9f751d3df00502f0
León Martín <leonmartinl@icloud.com>
2025-12-14 15:49:55 +0100

    update api url

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react'
 import './App.css'
 
-const API_URL = 'http://localhost:3000/api'
+const API_URL = 'https://api.leonmartin.cc/robe'
 
 function App() {
   const [messages, setMessages] = useState([])
